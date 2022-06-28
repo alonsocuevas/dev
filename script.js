c135af7b9ec604a308310723c6dbf709dc093876
Alonso <alonsocuevaspizarro@gmail.com>
2022-06-28 15:41:27 -0400

    mas cambios

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programador Web", "Desarrollador Backend"],
+        strings: ["Programador Web ", "Desarrollador Backend"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
