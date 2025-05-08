f7629f6262ff4f3929c4fb11df077cdddda82e4d
alonso <alonso.cuevas@nappinformatica>
2025-05-08 01:03:45 -0400

    nuevos arreglos 08-05-2025

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Ingeniero en Informática"],
+        strings: ["Alonso Cuevas Pizarro"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
