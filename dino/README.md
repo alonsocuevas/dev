8cf6f602ff9c303e6092b21ab875464cf03bce8e
alonsocuevas <alonsocuevaspizarro@gmail.com>
2025-07-09 00:23:31 -0400

    se agrego un dino juego version web jugable, se cambio un texto en lavista titulos y una direccion web que te lleva a termometro app web

--- dino/README.md
+++
@@ -1,6 +0,0 @@
-# FolioWeb
-Mi primera pagina web
-
-este es mi primera pagina web en la cual muestro  un poco de mi desempeño, trabajos y cosas que yo realizo.
-
-esta pagina fue creada  por mi Alonso Cuevas Pizarro  y  a ella tambien adjunte moldes que me hicieron mas facil el trabajo 
--- titulos/script.js
+++ titulos/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Certificados de titulos profesionales obtenidos"],
+        strings: ["Titulos profesionales"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
